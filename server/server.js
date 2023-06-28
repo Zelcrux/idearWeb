5f9924b1852531e9fd61247b3de26df92a96c64f
ThanonC <126615459+ThanonC@users.noreply.github.com>
2023-06-28 06:48:33 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { body, validationResult } = require("express-validator");
 var app = express()
 var morgen = require('morgan')
 const { EmbedBuilder, WebhookClient, Embed } = require('discord.js');
-const webhookClient = new WebhookClient({ url: 'https://discord.com/api/webhooks/1123286457780551701/VWRq4VLZC_ODLO5AEnpTLZwnbCAgZEeCVK8i-XxYEu6NpQu5IfsZ7CurFJO_-l6OOc6h' });
+const webhookClient = new WebhookClient({ url: 'change pls' });
 
 app.use(morgen('dev'))
 app.use(express.urlencoded({
